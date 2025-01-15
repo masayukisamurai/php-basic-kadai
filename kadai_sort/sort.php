c2309fe43f2a7a8d140d7e828fab694c53153738
yuki <maaboo0812@gmail.com>
2025-01-15 14:31:30 +0900

    「PHPマニュアルを参照してソート関数を実装してみよう」の課題。sort_2way関数を引数に使うために変更。

--- kadai_sort/sort.php
+++ kadai_sort/sort.php
@@ -7,28 +7,33 @@
 </head>
 
 <body>
-<p> 
-  <?php
+<p>
+<?php
     $sort_2way = function ($nums, $order) {
         if ($order) {
-          echo "昇順にソートします。<br>";
-          sort($nums);
-          foreach ($nums as $array) {
-            echo $array . "<br>";
-          }
+            echo "昇順にソートします。<br>";
+            sort($nums);
         } else {
-          echo "降順にソートします。<br>";
-          rsort($nums);
-          foreach ($nums as $array) {
-            echo $array . "<br>";
-          }
+            echo "降順にソートします。<br>";
+            rsort($nums);
         }
-      };
 
-  $nums = [15, 4, 18, 23, 10];
-  $sort_2way($nums, true);
-  $sort_2way($nums, false);
-  ?>
+        foreach ($nums as $num) {
+            echo $num . "<br>";
+        }
+    };
+
+    function process_sort_function($sortFunction, $nums, $order) {
+        echo "関数を引数として受け取りました:<br>";
+        $sortFunction($nums, $order);
+        echo "処理が完了しました。<br><br>";
+    }
+
+
+    $nums = [15, 4, 18, 23, 10];
+    process_sort_function($sort_2way, $nums, true);  // 昇順ソート
+    process_sort_function($sort_2way, $nums, false); // 降順ソート
+?>
 </p>
 </body>
 
